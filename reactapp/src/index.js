b649d88c14c39a99067796b79e729b6d9546c0ea
priskisam <priskisam@gmail.com>
2022-07-11 16:32:16 +0000

    commit

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import HomePage from './components/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <HomePage />
+  </Provider>,
+  document.getElementById('root')
+);
